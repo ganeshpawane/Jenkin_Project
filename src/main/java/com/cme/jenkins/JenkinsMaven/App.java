3a1aba71f87f2472c6f3858b49bdd3d4c2f8ad27
ganeshpawane <ganeshpawane4@gmail.com>
2017-05-31 15:31:19 +0530

    Added from eclipse

---
+++ src/main/java/com/cme/jenkins/JenkinsMaven/App.java
@@ -0,0 +1,13 @@
+package com.cme.jenkins.JenkinsMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello ganesh!" );
+    }
+}
