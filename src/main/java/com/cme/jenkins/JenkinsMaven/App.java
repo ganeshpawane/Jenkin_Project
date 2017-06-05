65e68490157f3123d85dd24b39f126c6abb8b6f9
ganeshpawane <ganeshpawane4@gmail.com>
2017-06-05 12:18:00 +0530

    Message Updated

--- src/main/java/com/cme/jenkins/JenkinsMaven/App.java
+++ src/main/java/com/cme/jenkins/JenkinsMaven/App.java
@@ -5,7 +5,7 @@ public class App
     public static void main( String[] args )
     {
     	App app = new App();
-        System.out.println( "Project Build Successfully" );
+        System.out.println( "Build Successfully" );
         System.out.println("Addition is : " + app.addition());
     }
     
